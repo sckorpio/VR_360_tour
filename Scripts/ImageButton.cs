0e0010ea4afd4e80fd564202ab63ae12201251e6
Kartik kumar <kartik.sckorpio@gmail.com>
2018-05-08 13:03:14 +0530

    Add files via upload

---
+++ Scripts/ImageButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageButton : MonoBehaviour 
+{
+	public Image IB;
+	public Image IB2;
+	public Text T;
+	// Use this for initialization
+	void Start () 
+	{
+		IB2.enabled = false;
+		T.enabled = false;
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	public void OutSide()
+	{
+		IB.color = Color.yellow;
+		IB2.enabled = false;
+		T.enabled = false;
+	}
+
+	public void Inside()
+	{
+		IB.color = Color.red;
+		IB2.enabled = true;
+		T.enabled = true;
+	}
+}
